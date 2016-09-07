d8ba8f999533d829dca97ac04c4810317c893e49
Itir Karac <karacitir@gmail.com>
2016-09-07 13:57:36 +0300

    Project template created

---
+++ src/SudokuVerifier.java
@@ -0,0 +1,10 @@
+
+public class SudokuVerifier {
+	
+	public int verify(String candidateSolution) {
+		// returns 0 if the candidate solution is correct
+		return 0;
+		
+		//check
+	}
+}
---
+++ tests/SudokuVerifierTest.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SudokuVerifierTest {
+
+// A correct Sudoku string: 417369825632158947958724316825437169791586432346912758289643571573291684164875293
+// An incorrect Sudoku string: 123456789912345678891234567789123456678912345567891234456789123345678912234567891
+
+	@Test
+	public void testVerify() {
+		fail("Not yet implemented");
+	}
+
+}
